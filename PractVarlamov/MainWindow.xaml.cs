bf7ffc0b2aa3b4fcc4b72097d77c572b4dbd7903
исп-44 <-44@LAB25-09>
2023-12-14 17:06:07 +0300

    Добавьте файлы проекта.

---
+++ PractVarlamov/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PractVarlamov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Height += 25;
+            int[] mas = new int[5];
+            DataGrid1.ItemsSource = mas.ToDataTable().DefaultView;
+        }
+
+        private void Enter(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
