c5bad44cfb85d951214ced43c821ac18f484c60c
snarix <sanr@mail.ru>
2023-12-14 23:38:33 +0300

    dop

--- PractVarlamov/MainWindow.xaml.cs
+++ PractVarlamov/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Security.Claims;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,17 +18,43 @@ namespace PractVarlamov
     /// </summary>
     public partial class MainWindow : Window
     {
+        int[] mas = new int[5];
+
         public MainWindow()
         {
             InitializeComponent();
             this.Height += 25;
-            int[] mas = new int[5];
             DataGrid1.ItemsSource = mas.ToDataTable().DefaultView;
         }
 
         private void Enter(object sender, RoutedEventArgs e)
         {
-            
+            var selectedRow = DataGrid1.SelectedItem as DataRowView;
+
+            if (selectedRow != null)
+            {
+                int count = 0;
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    mas[i] = Convert.ToInt32(selectedRow.Row.ItemArray[i]);
+                    count++;
+                }
+
+                Class class1 = new(mas);
+
+                if (class1.Task())
+                {
+                    MessageBox.Show("Последовательность имеет все простые числа");
+                }
+                else
+                {
+                    MessageBox.Show("Последовательность имеет не простые числа");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заполните DataGrid");
+            }
         }
     }
 }
